28e8df59a9f9a10baf37b96d1bffea1eb59d57b5
bangsyir <bangsyir@gmail.com>
2025-10-28 22:12:22 +0800

    add user logged in view

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,16 +1,33 @@
-import { GalleryVerticalEnd } from "lucide-react";
+import {
+  ChevronsDown,
+  GalleryVerticalEnd,
+  LogOut,
+  Settings,
+} from "lucide-react";
 import { Link } from "react-router";
 import { ModeToggle } from "./theme-toggle";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useAuthUserStore } from "~/store/authStore";
+import { Button, buttonVariants } from "./ui/button";
+import { cn } from "~/lib/utils";
+import { Logout } from "./logout";
 
-export function Navbar() {
+export function Navbar({ isLogin }: { isLogin: boolean | undefined }) {
   return (
-    <div className="border-border bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 flex items-center justify-between border-b pt-2 pb-2 backdrop-blur">
+    <div className="sticky top-0 z-50 flex items-center justify-between border-b border-border bg-background/95 pt-2 pb-2 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div>
         <Link
           to="/"
           className="flex items-center gap-2 self-center font-medium"
         >
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <GalleryVerticalEnd className="size-4" />
           </div>
           sifokus.
@@ -18,7 +35,51 @@ export function Navbar() {
       </div>
       <div className="flex items-center gap-2">
         <ModeToggle />
+
+        {!isLogin ? (
+          <Link to="/signin" className={buttonVariants({ variant: "outline" })}>
+            Signin
+          </Link>
+        ) : (
+          <SignedInUserDropDown />
+        )}
       </div>
     </div>
+  );
+}
+
+function SignedInUserDropDown() {
+  const authUser = useAuthUserStore((s) => s.authUser);
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          <Avatar>
+            <AvatarImage
+              src={authUser?.avatarUrl || undefined}
+              alt="@evilrabbit"
+            />
+            <AvatarFallback>{authUser?.username?.slice(0, 2)}</AvatarFallback>
+          </Avatar>
+          <p>{authUser?.username}</p>
+          <ChevronsDown />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuItem asChild>
+          <Link to="/settings" className="flex w-full items-center gap-2">
+            <Settings />
+            Settings
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Logout className={cn("ml-0 flex w-full items-center gap-2")}>
+            <LogOut />
+            Sign out
+          </Logout>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
