b758ee0482fb5c0f6aa416966bd42de5b79f3ac1
bangsyir <bangsyir@gmail.com>
2025-10-28 22:13:13 +0800

    add set-theme route

---
+++ app/routes/set-theme.ts
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/set-theme";
+import { data } from "react-router";
+
+export async function action({ request }: Route.ActionArgs) {
+  const { commitSession, getSession } = await import("~/lib/theme.server");
+  const formData = await request.formData();
+  const theme = formData.get("theme");
+
+  if (typeof theme !== "string") {
+    throw new Error();
+  }
+  const session = await getSession(request.headers.get("Cookie"));
+  session.set("theme", theme);
+
+  return data(null, {
+    headers: { "Set-Cookie": await commitSession(session) },
+  });
+}
